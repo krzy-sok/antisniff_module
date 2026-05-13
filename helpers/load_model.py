565c156d3a5b6bd01888cf151dca62e01bf543a9
krzy-sok <01178595@pw.edu.pl>
2026-05-13 11:43:34 +0200

    feat: wrapper class for sniffer classification models

--- helpers/load_model.py
+++ helpers/load_model.py
@@ -2,6 +2,6 @@ from onnxruntime import InferenceSession
 import numpy as np
 
 def load_model(name) -> InferenceSession:
-    with open("{name}.onnx", "rb") as f:
+    with open(f"{name}.onnx", "rb") as f:
         onx = f.read()
     return InferenceSession(onx, providers=["CPUExecutionProvider"])
--- main.py
+++ main.py
@@ -4,41 +4,43 @@ from helpers.load_model import load_model
 from random import random
 import uvicorn
 from onnxruntime import InferenceSession
+import os
 
 from helpers.labeler import Labeler
 from helpers.probe_row import ProbeRow
+from helpers.sniffer_classifier_context import SnifferClassifierContext
 
 app = FastAPI()
 
 @app.post("/predict")
 def predict(row: ProbeRow):
-    print(f"""arguments:\n
-            avg: {row.rtt_avg}\n
-            mean: {row.rtt_median},\n
-            flood: {row.flood_flag},\n
-            max_diff: {row.max_diff},\n
-            device: {row.device},\n
-            ip: {row.ip}""")
+    # print(f"""arguments:\n
+    #         avg: {row.rtt_avg}\n
+    #         mean: {row.rtt_median},\n
+    #         flood: {row.flood_flag},\n
+    #         max_diff: {row.max_diff},\n
+    #         device: {row.device},\n
+    #         ip: {row.ip}""")
     if row.flood_flag == 0:
         app.state.LABELER.label_machine(row)
     label = app.state.LABELER.get_label(row.device, row.ip)
+    if label is None:
+        return { "sniffing": -1.0, "label": "unknown"}
 
     if label == "laptop":
-        model: InferenceSession = app.state.MODEL_LAPTOP
+        model: SnifferClassifierContext = app.state.MODEL_LAPTOP
     else:
-        model: InferenceSession = app.state.MODEL_PC
+        model: SnifferClassifierContext = app.state.MODEL_PC
 
-    input = np.array([row.rtt_avg, row.rtt_median, float(row.flood_flag), row.max_diff])
-    input = input.astype("float32")
-    input = input.reshape(1,-1)
-    res = model.run(["output_probability"], {"input": input})
-    proba = res[0]
-    print(f"res:\n {res}\n")
-    return { "sniffing": proba[0][1], "label": label}
+    probability  = model.classify(row)
+    print(f"res:\n {probability}\n")
+    return { "sniffing": probability, "label": label}
 
 def main():
-    app.state.MODEL_LAPTOP = load_model("laptop_model")
-    app.state.MODEL_PC = load_model("pc_model")
+    flood_only = os.getenv("ANTISNIFFER_FLOOD_ONLY", True)
+    # flood_only = False
+    app.state.MODEL_LAPTOP = SnifferClassifierContext(flood_only, "laptop_model")
+    app.state.MODEL_PC = SnifferClassifierContext(flood_only, "pc_model")
     app.state.LABELER = Labeler()
     uvicorn.run(app, host="0.0.0.0", port = 8001)
 
