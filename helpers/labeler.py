fd1ef30fdbb35ce7f8dfeb93eb49db341c9df472
krzy-sok <01178595@pw.edu.pl>
2026-05-13 11:40:21 +0200

    fix: exception when attempting to get unset label

--- helpers/labeler.py
+++ helpers/labeler.py
@@ -18,14 +18,15 @@ class Labeler:
         input = input.astype("float32")
         input = input.reshape(1,-1)
         res = self.label_model.run(["output_label"], {"input": input})
-        # breakpoint()
+
         print(res[0].tolist()[0])
         if row.device not in self.label_map.keys():
             self.label_map.update({row.device: {row.ip : res[0].tolist()[0]}})
         else:
             self.label_map[row.device][row.ip] =  res[0].tolist()[0]
-        breakpoint()
-
 
     def get_label(self, device: str, ip: str):
-        return self.label_map.get(device).get(ip)
+        if (device_map := self.label_map.get(device)) is not None and (label := device_map.get(ip)) is not None:
+            return label
+        else:
+            return None
